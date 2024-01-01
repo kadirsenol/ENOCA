e5f9b3ec8a17ec3ca7ee0c3452b5d8a57fbcb9a4
kadirsenol <kdrsnl_61@hotmail.com>
2024-01-01 07:07:31 +0300

    Enoca calisanlar/proje database & Sorgu

---
+++ Soru7/query.sql
@@ -0,0 +1,7 @@
+--**(Bu örnekte chatGPT üzerinden biraz destek aldım.)**
+--"updatedAt:[1577836800000 TO *]" ifadesi, 2020 ocak ayından sonraki tarihleri temsil eder.
+--Unix epoch formatında 2020-01-01 tarihine denk gelmektedir.
+--Burada TO * ifadeside belirtileen tarihten sonraki tarihleri ifade eder.
+--İstenillen sorgu kısmı aşşağıda ki gibidir.
+
+http://example/select?q=updatedAt:[1577836800000 TO *]
